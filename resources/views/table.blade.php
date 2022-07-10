71ff795229f9ef385ecfe53747228b0f7de33142
dom-elves <79201054+dom-elves@users.noreply.github.com>
2022-07-10 20:04:30 +0100

    fixed an if statement

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -33,14 +33,14 @@
             <player-component v-bind:player="{{ json_encode($players[3]) }}" :hand="{{ json_encode($hand_4) }}"></player-component>
             @endif
 
-            if ($hand_5 != null)
+            @if ($hand_5 != null)
             <player-component v-bind:player="{{ json_encode($players[4]) }}" :hand="{{ json_encode($hand_5) }}"></player-component>
             @endif
 
             @if ($hand_6 != null)
             <player-component v-bind:player="{{ json_encode($players[5]) }}" :hand="{{ json_encode($hand_6) }}"></player-component>
             @endif
-            
+
         </div>
 
         
